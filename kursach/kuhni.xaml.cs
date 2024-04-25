5e7f25a473c29352479cd539df82e99bb9e4907d
yantripe <yantripe@DESKTOP-B82T1MO>
2024-04-25 18:34:37 +0300

    Добавьте файлы проекта.

---
+++ kursach/kuhni.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kursach
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class kuhni : Window
+    {
+        public kuhni()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            podboroboev taskWindow = new podboroboev();
+            taskWindow.Show();
+            Close();
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
